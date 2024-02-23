557e8416e14fb6b5cf8b8b5c1f92d38dd1f843fc
ice.29s <duong.ta290601@gmail.com>
2024-02-23 09:45:48 +0700

    Revert "AD commit"
    This reverts commit 0198a85a1941d5224b07cf468c425fede4c0f41e.

--- src/config/config.py
+++
@@ -1,7 +0,0 @@
-config_path = "C:/AD/Osint/playwright_tiktok/src/config/dev_config.json"
-search_post_tiktok = 'https://www.tiktok.com/search/video?q='
-hashtag_tiktok = 'https://www.tiktok.com/tag/'
-explore_tiktok = 'https://www.tiktok.com/explore'
-kafa_address ="172.168.200.202:9092"
-api_address='http://172.168.200.200:8000'
-url_tiktok = "https://tiktok.com"
